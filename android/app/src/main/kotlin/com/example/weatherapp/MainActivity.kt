55310d8eb47fbb1037f70bac7ac2009a14a91db8
Sujit Singh <ssingh5@wayfair.com>
2020-05-05 22:39:42 -0400

    first commit

--- android/app/src/main/kotlin/com/example/weatherapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/weatherapp/MainActivity.kt
@@ -4,9 +4,18 @@ import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+
+    private val CHANNEL = "com.wayfair.flutter/android"
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
+
+        MethodChannel(flutterEngine.dartExecutor, CHANNEL).setMethodCallHandler { methodCall, result ->
+            if (methodCall.method == "printAndroidOutput") {
+                result.success("hello from android world")
+            }
+        }
     }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
@@ -46,6 +47,18 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  static const platform = const MethodChannel("com.wayfair.flutter/android");
+
+  void printAndroidOutput() async {
+    String value;
+    try {
+      value = await platform.invokeMethod("printAndroidOutput");
+    } catch (e) {
+      print(e);
+    }
+    print(value);
+  }
+
   void _incrementCounter() {
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -53,6 +66,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
+      printAndroidOutput();
       _counter++;
     });
   }
